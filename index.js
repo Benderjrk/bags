b644c9cb3909c565d77e4895d40ac788349dfb9a
Justin Bender <Jrkbender@gmail.com>
2020-04-08 15:09:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -20,6 +20,23 @@ function createPackage(small, big, goal) {
 
     } else if (totalKilo - big*5 < 0) {
 
+      for (var i = 0; i < big; i++) {
+        totalKilo = totalKilo - 5;
+
+        if (totalKilo - 5 > 0) {
+
+          for (var t = 0; t < small; t++) {
+            console.log(totalKilo);
+            totalKilo = totalKilo - 1;
+
+            if (totalKilo === 0) {
+              return t+1;
+            }
+          }
+
+        }
+      }
+
     } else if (totalKilo - big*5 === 0) {
       return 0;
     }
